7ad3ca003d33a54db11a58b037e5f6290752b370
ivan.hryshko <ihryshko@datrics.ai>
2022-12-19 00:28:22 +0200

    disable public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/history-of-ukrainian-war-memes/',
+  // publicPath: '/history-of-ukrainian-war-memes/',
   transpileDependencies: true,
   chainWebpack: config => config.resolve.symlinks(false),
 })
