5a703168600c4f7bc832b1c9668520c37d23cd31
ivan.hryshko <ihryshko@datrics.ai>
2023-01-07 15:35:18 +0200

    feat: added google analytic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
-createApp(App).use(router)
+createApp(App)
+  .use(router)
+  .use(VueAnalytics, {
+    id: 'G-16Z655D45G',
+    router
+  })
   .mount('#app')
